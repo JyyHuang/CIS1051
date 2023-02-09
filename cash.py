e10e5c9352ddd2d309f3d3ab86b0114c4776032f
Joey Huang <joeyh6547@gmail.com>
2023-02-09 18:10:51 -0500

    Started lab 6

--- cash.py
+++ cash.py
@@ -1,11 +1,37 @@
 def isFloat(string):
-
+    try:
+        value = (float(string))
+        if value >= 0:
+            return True
+    except:
+        return False
 # User Input
 while True:
-    try:
-        total = input("Total amount: ")
-        if isFloat(total) == True:
-            break
-    except:
-        if isFloat(total) == False:
-            continue
+    change = input("Change owed: ")
+    if isFloat(change) == True:
+        change = float(change)
+        break
+    else:
+        continue
+
+cents = round(int(change * 100)) # turn dollars into cents
+counter = 0 
+while True:
+    if cents >= 25:
+        cents -= 25
+        counter += 1
+    elif cents >= 10:
+        cents -= 10
+        counter += 1
+    elif cents >= 5:
+        cents -= 5
+        counter += 1
+    elif cents >= 1:
+        cents -= 1
+        counter += 1
+    else:
+        break
+print(counter)
+
+    
+

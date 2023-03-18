8c93bc1ad195b9986dd20beeec5a35569f97bb1c
Joey Huang <joeyh6547@gmail.com>
2023-03-17 23:21:06 -0400

    HawaiianLab

--- Hawaiian/hawaiianwords.py
+++ Hawaiian/hawaiianwords.py
@@ -3,11 +3,11 @@ wdict = {
 }
 
 voweldict = {
-    "a" : "ah",
-    "e" : "eh",
-    "i" : "ee",
-    "o" : "oh",
-    "u" : "oo"
+    "a" : "ah-",
+    "e" : "eh-",
+    "i" : "ee-",
+    "o" : "oh-",
+    "u" : "oo-"
 }
 
 consonantdict = {
@@ -22,45 +22,39 @@ consonantdict = {
 }
 
 groupvoweldict = {
-    "ai" : "eye",
-    "ae" : "eye",
-    "ao" : "ow",
-    "au" : "ow",
-    "ei" : "ay",
-    "eu" : "eh-oo",
-    "iu" : "ew",
-    "oi" : "oyo",
-    "ou" : "ow",
-    "ui" : "ooey"
+    "ai" : "eye-",
+    "ae" : "eye-",
+    "ao" : "ow-",
+    "au" : "ow-",
+    "ei" : "ay-",
+    "eu" : "eh-oo-",
+    "iu" : "ew-",
+    "oi" : "oyo-",
+    "ou" : "ow-",
+    "ui" : "ooey-"
 }
 
 
 def main():
     string = check_input()
-    
-    for index, char in enumerate(string):
-
-        next_char = string[min((index + 1, len(string)-1))]
-
-        if char in consonantdict:
-            print(consonantdict[char], end="")
-
-        elif char in wdict:
-            if char == "w" and string[index - 1] == "i" or string[index - 1] == "e":
-                print(wdict[char][1], end="")
-            else:
-                print(wdict[char][0], end="")
-
-        elif char in voweldict:
-            if char + next_char in groupvoweldict:
-                print(groupvoweldict[char + next_char] + "-", end="")
-                char = 0
-
-            elif index != len(string) - 1:
-                print(voweldict[char] + "-", end="")
-            else:
-                print(voweldict[char], end="")
-
+    word = pronounce(string)
+    word = rmHyphen(word)
+    print(word)
+    valid = True
+    while valid:
+        question = input("Do you want to enter another word? Y/YES/N/NO ")
+        if question.lower() not in ["yes", "y", "no", "n"]:
+            print("Enter Y, YES, N, or NO")
+            continue
+        elif question.lower() == "y" or question.lower() == "yes":      # If yes, then does the steps again
+            string = check_input()
+            word = pronounce(string)
+            word = rmHyphen(word)
+            print(word)
+        else:
+            valid = False
+            
+    return word
 
 
 def check_input():
@@ -76,6 +70,51 @@ def check_input():
             else:
                 print(f"{char} is not a valid hawaiian character")
                 valid = True
+                counter = 0
     return word
+
+
+def pronounce(string):
+    result = ""
+    index = 0
+    while index < len(string):
+
+        char = string[index].lower()
+
+        if char in consonantdict:
+            result += consonantdict[char]
+
+        elif char in wdict:
+            if char == "w" and string[index - 1] == "i" or string[index - 1] == "e":
+                result += wdict[char][1]
+            else:
+                result += wdict[char][0]
+        
+        elif char in voweldict:
+            if index != len(string) - 1 and char + string[index + 1] in groupvoweldict:     # looks at the char and the next char
+                result += groupvoweldict[char + string[index + 1]]
+                index += 1         # skips a char
+            else:
+                result += voweldict[char]
+
+        index += 1
+    return result.rstrip("-").capitalize()
+
+
+# This function removes unwanted hyphens
+# If there is a space or apostrophe in front of the hyphen, then it is removed
+def rmHyphen(string):
+    index = 0
+    space = "- "
+    apostrophe = "-'"
+    while index < len(string) - 1:
+        char = string[index]
+        if char + string[index + 1] == space:
+            string = string.replace(space, " ")
+        elif char + string[index + 1] == apostrophe:
+            string = string.replace(apostrophe, "'")
+        index += 1
+    return string
+
 
 main()
